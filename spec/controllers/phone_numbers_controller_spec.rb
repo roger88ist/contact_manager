8cf1a444109e30f8cf4bde226b0cb0678be9c73a
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-07-05 01:13:45 -0400

    made changes to phone_number controller update method so that it can redirect to the person's show page. Added tests and fixed let statements in spec files to test update action.

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -108,32 +108,37 @@ RSpec.describe PhoneNumbersController, type: :controller do
   end
 
   describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        {number: 'MyNewString', person_id: 2}
-      }
+    describe "with valid params" do
 
-      it "updates the requested phone_number" do
-        phone_number = PhoneNumber.create! valid_attributes
-        put :update, {id: phone_number.to_param, phone_number: new_attributes, session: valid_session}
-        phone_number.reload
-        expect(phone_number.number).to eq('MyNewString')
-        expect(phone_number.person_id).to eq(2)
-      end
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {number: '555-5678', person_id: bob.id} }
+      let(:new_attributes) { {number: 'MyNewString', person_id: bob.id} }
 
-      it "assigns the requested phone_number as @phone_number" do
-        phone_number = PhoneNumber.create! valid_attributes
-        put :update, {id: phone_number.to_param, phone_number: valid_attributes, session: valid_session}
-        expect(assigns(:phone_number)).to eq(phone_number)
-      end
+      context "with valid params" do
 
-      it "redirects to the phone_number" do
-        phone_number = PhoneNumber.create! valid_attributes
-        put :update, {id: phone_number.to_param, phone_number: valid_attributes, session: valid_session}
-        expect(response).to redirect_to(phone_number)
+        it "updates the requested phone_number" do
+          phone_number = PhoneNumber.create! valid_attributes
+          put :update, {id: phone_number.to_param, phone_number: new_attributes, session: valid_session}
+          phone_number.reload
+          expect(phone_number.number).to eq('MyNewString')
+          expect(phone_number.person_id).to eq(bob.id)
+        end
+
+        it "assigns the requested phone_number as @phone_number" do
+          phone_number = PhoneNumber.create! valid_attributes
+          put :update, {id: phone_number.to_param, phone_number: valid_attributes, session: valid_session}
+          expect(assigns(:phone_number)).to eq(phone_number)
+        end
+
+        it "redirects to the phone_number" do
+          bob = Person.create(first_name: 'Bob', last_name: 'Jones')
+          valid_attributes = {number: '555-5678', person_id: bob.id}
+          phone_number = PhoneNumber.create! valid_attributes
+          put :update, {id: phone_number.to_param, phone_number: valid_attributes, session: valid_session}
+          expect(response).to redirect_to(bob)
+        end
       end
     end
-
     context "with invalid params" do
       it "assigns the phone_number as @phone_number" do
         phone_number = PhoneNumber.create! valid_attributes
